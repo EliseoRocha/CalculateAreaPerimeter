6e7e8450426234e848c63af792966b0b0ee259de
Eliseo Rocha <rochaeliseo91@gmail.com>
2018-10-29 15:52:34 -0700

    Initial commit

---
+++ CalculateAreaPerimeter/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace CalculateAreaPerimeter
+{
+    partial class CalculateAreaAndPerimeter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LengthTextBox = new System.Windows.Forms.TextBox();
+            this.WidthTextBox = new System.Windows.Forms.TextBox();
+            this.AreaTextBox = new System.Windows.Forms.TextBox();
+            this.PerimeterTextBox = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.LengthLabel = new System.Windows.Forms.Label();
+            this.WidthLabel = new System.Windows.Forms.Label();
+            this.AreaLabel = new System.Windows.Forms.Label();
+            this.PerimeterLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LengthTextBox
+            // 
+            this.LengthTextBox.Location = new System.Drawing.Point(223, 71);
+            this.LengthTextBox.Name = "LengthTextBox";
+            this.LengthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.LengthTextBox.TabIndex = 0;
+            this.LengthTextBox.TextChanged += new System.EventHandler(this.LengthTextBox_TextChanged);
+            // 
+            // WidthTextBox
+            // 
+            this.WidthTextBox.Location = new System.Drawing.Point(223, 125);
+            this.WidthTextBox.Name = "WidthTextBox";
+            this.WidthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.WidthTextBox.TabIndex = 1;
+            this.WidthTextBox.TextChanged += new System.EventHandler(this.WidthTextBox_TextChanged);
+            // 
+            // AreaTextBox
+            // 
+            this.AreaTextBox.Location = new System.Drawing.Point(223, 181);
+            this.AreaTextBox.Name = "AreaTextBox";
+            this.AreaTextBox.ReadOnly = true;
+            this.AreaTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AreaTextBox.TabIndex = 2;
+            // 
+            // PerimeterTextBox
+            // 
+            this.PerimeterTextBox.Location = new System.Drawing.Point(223, 242);
+            this.PerimeterTextBox.Name = "PerimeterTextBox";
+            this.PerimeterTextBox.ReadOnly = true;
+            this.PerimeterTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PerimeterTextBox.TabIndex = 3;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(145, 302);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(75, 23);
+            this.CalculateButton.TabIndex = 4;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(248, 302);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 5;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // LengthLabel
+            // 
+            this.LengthLabel.AutoSize = true;
+            this.LengthLabel.Location = new System.Drawing.Point(142, 71);
+            this.LengthLabel.Name = "LengthLabel";
+            this.LengthLabel.Size = new System.Drawing.Size(43, 13);
+            this.LengthLabel.TabIndex = 6;
+            this.LengthLabel.Text = "Length:";
+            // 
+            // WidthLabel
+            // 
+            this.WidthLabel.AutoSize = true;
+            this.WidthLabel.Location = new System.Drawing.Point(145, 125);
+            this.WidthLabel.Name = "WidthLabel";
+            this.WidthLabel.Size = new System.Drawing.Size(38, 13);
+            this.WidthLabel.TabIndex = 7;
+            this.WidthLabel.Text = "Width:";
+            // 
+            // AreaLabel
+            // 
+            this.AreaLabel.AutoSize = true;
+            this.AreaLabel.Location = new System.Drawing.Point(145, 181);
+            this.AreaLabel.Name = "AreaLabel";
+            this.AreaLabel.Size = new System.Drawing.Size(32, 13);
+            this.AreaLabel.TabIndex = 8;
+            this.AreaLabel.Text = "Area:";
+            // 
+            // PerimeterLabel
+            // 
+            this.PerimeterLabel.AutoSize = true;
+            this.PerimeterLabel.Location = new System.Drawing.Point(145, 242);
+            this.PerimeterLabel.Name = "PerimeterLabel";
+            this.PerimeterLabel.Size = new System.Drawing.Size(54, 13);
+            this.PerimeterLabel.TabIndex = 9;
+            this.PerimeterLabel.Text = "Perimeter:";
+            // 
+            // CalculateAreaAndPerimeter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 400);
+            this.Controls.Add(this.PerimeterLabel);
+            this.Controls.Add(this.AreaLabel);
+            this.Controls.Add(this.WidthLabel);
+            this.Controls.Add(this.LengthLabel);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.PerimeterTextBox);
+            this.Controls.Add(this.AreaTextBox);
+            this.Controls.Add(this.WidthTextBox);
+            this.Controls.Add(this.LengthTextBox);
+            this.Name = "CalculateAreaAndPerimeter";
+            this.Text = "CalculateAreaAndPerimeter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LengthTextBox;
+        private System.Windows.Forms.TextBox WidthTextBox;
+        private System.Windows.Forms.TextBox AreaTextBox;
+        private System.Windows.Forms.TextBox PerimeterTextBox;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label LengthLabel;
+        private System.Windows.Forms.Label WidthLabel;
+        private System.Windows.Forms.Label AreaLabel;
+        private System.Windows.Forms.Label PerimeterLabel;
+    }
+}
+
---
+++ CalculateAreaPerimeter/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateAreaPerimeter
+{
+    public partial class CalculateAreaAndPerimeter : Form
+    {
+        public CalculateAreaAndPerimeter()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            double length = Convert.ToDouble(LengthTextBox.Text);
+            double width = Convert.ToDouble(WidthTextBox.Text);
+            double area = FindArea(length, width);
+            double perimeter = FindPerimeter(length, width);
+            AreaTextBox.Text = area.ToString();
+            PerimeterTextBox.Text = perimeter.ToString();
+        }
+
+        private static double FindPerimeter(double length, double width)
+        {
+            return 2 * width + 2 * length;
+        }
+
+        private static double FindArea(double length, double width)
+        {
+            return width * length;
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void LengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            AreaTextBox.Clear();
+            PerimeterTextBox.Clear();
+        }
+
+        private void WidthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            AreaTextBox.Clear();
+            PerimeterTextBox.Clear();
+        }
+    }
+}
